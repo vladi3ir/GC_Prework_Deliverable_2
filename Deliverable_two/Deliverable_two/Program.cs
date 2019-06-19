8a582c4f190ba7ff84906772ac33b6b3529dfb4b
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-06-18 21:51:07 -0400

    complete

--- Deliverable_two/Deliverable_two/Program.cs
+++ Deliverable_two/Deliverable_two/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Deliverable_two
 {
@@ -10,6 +6,35 @@ namespace Deliverable_two
     {
         static void Main(string[] args)
         {
+            String question = @"Hello friend, what are you in the mood for?
+            Here are your options:
+            1)Action
+            2)Chill times
+            3)Danger
+            4)Good Food";
+            Console.WriteLine(question);
+
+            string mood = Console.ReadLine();
+
+            Console.WriteLine("How many people are you brining with you?");
+            string npeople = Console.ReadLine();
+
+            Int32 a;
+            Int32 b;
+
+            a = Convert.ToInt32(mood);
+            b = Convert.ToInt32(npeople);
+
+            string[] activity;
+            string[] vehicle;
+
+            activity = new string[4] { "Stock Car Racing", "Hiking", "Skydiving", "go to Taco Bell" };
+            vehicle = new string[4] { "sneakers", "a sedan", "a volkswagen bus", "an airplane" };
+
+            Console.WriteLine("You should go {0} and travel in {1}", activity[a - 1], vehicle[b - 1]);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+
         }
     }
 }
