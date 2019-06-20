da3c53ff3cbceeb168e99b3f513e72bca4b4498b
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-06-20 08:58:06 -0400

    This version does comes to the same conclusion with an if statement, it has more lines of code but its another way to do it

--- Deliverable_two/Deliverable_two/Program.cs
+++ Deliverable_two/Deliverable_two/Program.cs
@@ -24,14 +24,55 @@ namespace Deliverable_two
 
             a = Convert.ToInt32(mood);
             b = Convert.ToInt32(npeople);
+             
+            if (a == 1)
+            {
+                Console.WriteLine("You shouls go Stock Car Racing");
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine("You should go Hiking");
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine("You should go Skydiving");
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine("You should go to Taco Bell");
+            }
 
+
+
+            if (b == 1)
+            {
+                Console.WriteLine("You should wear sneakers");
+
+            }
+            else if (b == 2)
+            {
+                Console.WriteLine("You should go in a sedan");
+            }
+            else if (b == 3)
+            {
+                Console.WriteLine("You should go in a VW Bus");
+            }
+            else if (b == 4)
+            {
+                Console.WriteLine("You should go in an airplane");
+            }
+
+             /* Alternative solution that also works
+             
             string[] activity;
             string[] vehicle;
-
+            
             activity = new string[4] { "Stock Car Racing", "Hiking", "Skydiving", "go to Taco Bell" };
             vehicle = new string[4] { "sneakers", "a sedan", "a volkswagen bus", "an airplane" };
 
             Console.WriteLine("You should go {0} and travel in {1}", activity[a - 1], vehicle[b - 1]);
+            */
+
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
 
